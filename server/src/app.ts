1cb885ad6b0a67bf31d1cde8d7bf173b4f6be4c1
gBoole01 <68549849+gBoole01@users.noreply.github.com>
2023-01-08 16:46:37 +0100

    feat: implement socket.io on server side

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,12 +1,28 @@
 import express from 'express'
+import { createServer, Server as HttpServer } from 'http'
+import { Server } from 'socket.io'
 
 export default class App {
   public app: express.Application
   private port: number
+  private server: HttpServer
+  private io: Server
 
   constructor(port: number) {
     this.app = express()
+    this.server = createServer(this.app)
+    this.io = new Server(this.server, {
+      cors: { origin: 'http://localhost:5173' },
+    })
     this.port = port
+  }
+
+  public listenWebsocket() {
+    this.io.on('connection', (socket) => {
+      socket.join('test')
+      console.log(`New Connection on ${this.io} with socket ${socket}`)
+    })
+    console.log(`Initializing Websocket`)
   }
 
   public listen() {
--- server/src/server.ts
+++ server/src/server.ts
@@ -2,4 +2,5 @@ import App from './app'
 
 const app = new App(5000)
 
+app.listenWebsocket()
 app.listen()
